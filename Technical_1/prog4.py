6ed0b6a33104f389bd626fc3d31b23a5abe7d511
Yashwanth D <thewarwolfyd007@gmail.com>
2026-02-03 11:37:24 +0530

    Commit #3

--- Technical_1/prog4.py
+++ Technical_1/prog4.py
@@ -1,7 +1,16 @@
-def reverse(x, reversed_n = 0):
-    if x == 0:
-        return reversed_n
-    else:
-        return reverse(x//10, reversed_n * 10 + x % 10)
+# def reverse(x, reversed_n = 0):
+#     if x == 0:
+#         return reversed_n
+#     else:
+#         return reverse(x//10, reversed_n * 10 + x % 10)
     
-print(reverse(12345))
+# print(reverse(12345))
+
+def func(x, rev):
+    if x == 0:
+        return rev
+    rem = x % 10
+    rev = rev * 10 + rem
+    return func(x//10, rev)
+
+print(func(1234, rev=0))
